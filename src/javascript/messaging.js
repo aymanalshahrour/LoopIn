4c8c45f2946c66f80b9da7070b334f7cf7f9420c
aymanalshahrour <ayman.alshahrour@student.ucll.be>
2026-09-03 18:11:10 +0200

    msg send works + msg box clears

--- src/javascript/messaging.js
+++ src/javascript/messaging.js
@@ -46,5 +46,43 @@ function openChat(username) {
 
     // TODO: Load messages between currentUser and username
 }
+async function sendMessage() {
+    let sendMessageButton = document.getElementById("send-button");
+    let messageValue = document.getElementById("message-input").value;
+    let sendToUser = document.getElementById("active-chat-name").textContent;
+    const sender = localStorage.getItem('username');
 
+
+    console.log(sendMessageButton);
+    console.log(messageValue);
+    console.log(sendToUser);
+    console.log(sender);
+
+    let message = {
+        senderUsername: sender,
+        receiverUsername: sendToUser,
+        content: messageValue,
+    }
+
+    try {
+        const response = await fetch("http://localhost:8080/messages/send", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(message)
+        });
+
+        if (!response.ok) {
+            const errorMessage = await response.text();
+            throw new Error(errorMessage || "failed to send message");
+        }
+
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
+    document.getElementById("message-input").value = "";
+
+}
 loadContacts();
